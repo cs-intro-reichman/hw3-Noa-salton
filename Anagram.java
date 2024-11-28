b4d3bd88438e8f4321f801a286df1aa59e2c14d7
Noa Salton <noasalton@Noas-MacBook-Pro.local>
2024-11-28 15:32:53 +0200

    Anagram

--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,38 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		//String[] arrList1 = preProcess(str1).split("");
+		String str1ProProcess =  preProcess(str1);
+		String proPreProcessSTR2 = preProcess(str2);
+		if (proPreProcessSTR2.length() != str1ProProcess.length()){ return false;}
+		boolean bool = true;
+		for (int i=0; i < proPreProcessSTR2.length(); i++){
+			if (str1ProProcess.indexOf(proPreProcessSTR2.charAt(i)) == -1){bool = false;}
+		}
+		return bool;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		String lowerCase = "";
+		for (int i = 0 ; i < str.length(); i++){if (Character.isLetter(str.charAt(i))){lowerCase += str.charAt(i);}}
+		return lowerCase.toLowerCase();
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		String newstr = preProcess(str);
+		String arrANAGRAM [] = new String[newstr.length()];
+		String output = ""; 
+		for (int k = 0; k < arrANAGRAM.length; k++){arrANAGRAM[k] = "";} 
+		for (int i=0; i<newstr.length(); i++){
+			int rand = (int)(Math.random() * ((arrANAGRAM.length-1) + 1));
+			if (arrANAGRAM[rand] == ""){arrANAGRAM[rand] = arrANAGRAM[rand] += newstr.charAt(i);}
+			else{ while(arrANAGRAM[rand] != ""){rand = (int)(Math.random() * ((arrANAGRAM.length-1) + 1));}arrANAGRAM[rand] += newstr.charAt(i);}}
+		for (int j=0; j<arrANAGRAM.length; j++){output += arrANAGRAM[j];}
+		return output;
 	}
 }
